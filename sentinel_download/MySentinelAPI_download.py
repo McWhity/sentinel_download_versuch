6cf90f087c2bc6df74c9132f21d5df37e4c1ebdd
Thomas Weiß <t.weiss@iggf.geo.uni-muenchen.de>
2017-05-17 17:41:28 +0200

    user file to download sentinel data

---
+++ sentinel_download/MySentinelAPI_download.py
@@ -0,0 +1,96 @@
+# Please install python packages pycurl, homora and sentinelsat
+# The download from Scihub will fail if the server certificate cannot be verified because no default CA bundle is defined, as on Windows, or when the CA bundle is outdated. In most cases the easiest solution is to install or update `certifi <https: pypi.python.org="" pypi="" certifi="">`_: for example with 'pip install -U certifi'
+
+from MySentinelAPI import MySentinelAPI, all_in_one
+from datetime import datetime, date
+
+"""
+default settings
+-------------------------
+all_in_one(user, password, area, api_url='https://scihub.copernicus.eu/dhus', path='.', download='no', initial_date=None, end_date=datetime.now(),**keywords)
+
+Wildcards for restricting search query (keywords)
+-----------------------------------------------------------------
+* = any sequence of zero or more characters
+? = any one character
+
+needed keywords
+------------------------
+user : username of Sentinel Scientific Data Hub account (e.g. 'mustermann')
+password : password of Sentinel Scientific Data Hub account (e.g. '12345')
+area : area(s) of interest
+    input with coordinates (e.g. 'latmin, latmax, lonmin, lonmax') or
+    input with file for one or more areas (e.g. 'coordinates.geojson')
+api_url: url of Sentinel Scientific Data Hub
+path: path where to store the data
+initial_date (date when data was taken): start date of the query (e.g. '20160101'; datetime(2016, 1, 1, 12, 5); date(2016,1,1))
+end_date (date when data was taken): end date of the query (e.g. '20160102'; datetime(2016, 1, 2, 12, 5); date(2016,1,2))
+download :
+    'no' only query but no download of the data (default setting)
+    'yes' query and download all results of the query
+
+additional keywords Sentinel 1 and 2
+--------------------------------------------------
+platformname: e.g. 'Sentinel-1'; 'Sentinel-2'; 'Sentinel-1 OR Sentinel-2'
+filename: e.g 'S1A_EW*'; 'S1A_EW_GRDH_1SDH_20141003T003840_20141003T003920_002658_002F54_4DD1'; '*1SD?_20141003T003840*'
+orbitnumber: e.g. '20'; '[1020 TO 1021]'
+orbitdirection: e.g. 'ascending'; 'decending'
+producttype: e.g. 'SLC'; 'GRD'; 'OCN'; 'RAW'; 'SLC OR GRD'; ...
+relativeorbitnumber: e.g. '20'; '[1020 TO 1021]'
+sensoroperationalmode: e.g. 'SM'; 'IW'; 'EW'; 'SM OR IW'; ...
+
+additional keywords Sentinel 1 only
+-----------------------------------------------
+lastorbitnumber: e.g. '20'; '[1020 TO 1021]'
+polarisationmode: e.g. 'HH'; 'VV'; 'HV'; 'VH'; 'HH HV'; 'VV VH'; 'HH OR VV VH'; ...
+lastrelativeorbitnumber: e.g. '20'; '[1020 TO 1021]'
+swathidentifier: e.g. 'S1', 'IW1', 'EW4', 'S1 OR S2 OR S3', 'S? NOT S1'
+
+additional keywords Sentinel 2 only
+-----------------------------------------------
+cloudcoverpercentage: e.g. '[0 TO 40]'
+
+for possible values and probably more parameters please visit
+https://scihub.copernicus.eu/twiki/do/view/SciHubUserGuide/3FullTextSearch#Search_Keywords
+"""
+
+print(datetime.now())
+
+# needed keywords
+user = 'user'
+password = 'password'
+area = 'coordinates_wallerfing.geojson'
+api_url = 'https://scihub.copernicus.eu/apihub/'
+path = '/media/tweiss/Daten'
+initial_date = '20170514'
+end_date = datetime.now()
+download = 'yes'
+
+# additional keywords Sentinel 1 and 2
+platformname = '*'
+filename = '*'
+orbitnumber = '*'
+orbitdirection = '*'
+producttype = '*'
+relativeorbitnumber = '*'
+sensoroperationalmode = '*'
+
+# additional keywords Sentinel 1 only
+lastorbitnumber = '*'
+polarisationmode = '*'
+lastrelativeorbitnumber = '*'
+swathidentifier = '*'
+
+# additional keywords Sentinel 2 only
+cloudcoverpercentage = '*'
+
+result_sentinel1 = all_in_one(user, password, area=area, api_url=api_url, path=path, initial_date=initial_date, end_date=end_date, download=download, platformname=platformname, filename=filename, orbitnumber=orbitnumber, lastorbitnumber=lastorbitnumber, orbitdirection=orbitdirection, polarisationmode=polarisationmode, producttype=producttype, relativeorbitnumber=relativeorbitnumber, lastrelativeorbitnumber=lastrelativeorbitnumber, sensoroperationalmode=sensoroperationalmode, swathidentifier=swathidentifier)
+
+result_sentinel2 = all_in_one(user, password, area=area, api_url=api_url, path=path, initial_date=initial_date, end_date=end_date, download=download, platformname=platformname, filename=filename, orbitnumber=orbitnumber, orbitdirection=orbitdirection, producttype=producttype, relativeorbitnumber=relativeorbitnumber, sensoroperationalmode=sensoroperationalmode, cloudcoverpercentage=cloudcoverpercentage)
+
+print(datetime.now())
+
+
+
+
+
